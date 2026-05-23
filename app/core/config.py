985315fd6ef31ad9509b8ac307b382a2cae01b57
peachjelly13 <ananyasharma112233441234@gmail.com>
2026-05-23 11:57:53 +0000

    chore: project structure, config and dependencies setup

---
+++ app/core/config.py
@@ -0,0 +1,29 @@
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+class Settings(BaseSettings):
+    #App
+    APP_NAME: str
+    APP_ENV: str = "development"
+    DEBUG: bool = True
+    SECRET_KEY: str
+
+    # PostgreSQL
+    DATABASE_URL:str
+    
+    # Redis
+    REDIS_URL:str
+    
+    # Anthropic
+    ANTHROPIC_API_KEY:str
+   
+    # JWT
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ALGORITHM: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=True
+    )
+
+settings = Settings()
+
