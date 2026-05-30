74edf0561c45eed14a3f3b5beb55064234edad38
peachjelly13 <ananyasharma112233441234@gmail.com>
2026-05-30 10:15:47 +0000

    fix: added authorized_by back to PurchaseOrder

--- app/models/purchase_orders.py
+++ app/models/purchase_orders.py
@@ -55,5 +55,10 @@ class PurchaseOrder(Base, TimestampMixin, PrimaryKeyMixin):
         nullable=False,
         index=True
     )
-    
+    authorized_by: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("users.id"),
+        nullable=False,
+        index=True
+    )
 
